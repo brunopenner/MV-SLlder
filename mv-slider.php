7d6c8a3bbaf1195293b18fb4236933703e1f3f36
Bruno Penner <brunopenner01@gmail.com>
2024-06-25 07:33:55 +0800

    #14. Activate, deactivate and uninstall methods

--- mv-slider.php
+++ mv-slider.php
@@ -47,9 +47,26 @@ if (!class_exists('MV_Slider')) {
             define('MV_SLIDER_URL', plugin_dir_url(__FILE__));
             define('MV_SLIDER_VERSION', '1.0.0');
         }
+
+        public static function activate()
+        {
+            update_option('rewrite_rules', '');
+        }
+
+        public static function deactivate()
+        {
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall()
+        {
+        }
     }
 }
 
 if (class_exists('MV_Slider')) {
+    register_activation_hook(__FILE__, array('MV_slider', 'activate'));
+    register_deactivation_hook(__FILE__, array('MV_slider', 'deactivate'));
+    register_uninstall_hook(__FILE__, array('MV_slider', 'uninstall'));
     $mv_slider = new MV_Slider();
 }
