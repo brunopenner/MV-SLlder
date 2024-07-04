0b7bedbc276db81b959c166d9f08705d7fce526f
Bruno Penner <brunopenner01@gmail.com>
2024-07-04 11:51:47 +0800

    #38. Shortcode API - Downloading Flexslider's base files

---
+++ vendor/flexslider/flexslider.js
@@ -0,0 +1,10 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        touch: true,
+        directionNav: false,
+        smoothHeight: true,
+        controlNav: true,
+    });
+});
